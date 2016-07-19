cf1e97fa622d0c5e0b7171df33213a04112999b0
Hampus Forsvall <forzvall@gmail.com>
2016-07-19 20:22:16 +0200

    App boilerplate

--- app/app.ts
+++ app/app.ts
@@ -1,18 +1,19 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import {DepartureComponent} from './pages/departures/departures.component';
 
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
+
 export class MyApp {
 
   private rootPage: any;
 
   constructor(private platform: Platform) {
-    this.rootPage = TabsPage;
+    this.rootPage = DepartureComponent;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
